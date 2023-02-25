8b9c51b2eca42bcebf1d27ed7f5f15e303963510
SUNNY-258 <amanmalviya258@gmail.com>
2023-02-25 17:17:10 +0530

    second commit

--- model/db.js
+++ model/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-mongoose
-    .connect("mongodb://127.0.0.1:27017/imagesDB")
-    .then(() => console.log("db connected!"))
-    .catch((err) => console.log(err.message));
+// mongoose
+//     .connect("mongodb://127.0.0.1:27017/imagesDB")
+//    
+mongoose.connect("mongodb+srv://amanmalviya258:Aman123@cluster0.4wpddzu.mongodb.net/?retryWrites=true&w=majority").then(() => console.log("db connected!")).catch((err) => console.log(err.message));;
